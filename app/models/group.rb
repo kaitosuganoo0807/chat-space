0a6d32b2b6f8ab3846acf0b6b7a55a17309e8ecb
kaitosuganoo0807 <k.0807roadster@gmail.com>
2019-11-15 13:02:41 +0900

    chat-space

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,8 +5,8 @@ class Group < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
-  def show_last_message
-    if (last_message = messages.last).present?
+  def show_last_message
+    if (last_message = messages.last).present?
       last_message.content? ? last_message.content : '画像が投稿されています'
     else
       'まだメッセージがありません'
